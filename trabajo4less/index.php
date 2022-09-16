5165a7466fe91f417bf60795213547637713d7cd
Osiris Gonzalez <tompsom98@gmail.com>
2022-09-16 02:28:52 -0600

    Add files via upload

---
+++ trabajo4less/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/main.css">
+    <title>Login</title>
+</head>
+    <body>
+        <div class ="container">
+                <fieldset>
+                        <form action="">
+                    <legend>
+                        Dar Acceso
+                    </legend>
+                    <label>
+                        User
+                    </label>
+                    <input type="text" name="" placehorder="write here">
+                    <label>
+                        Password
+                    </label>
+                    <input type="password" name="" placehorder="*****">
+                    <button type="submit">
+                        ACCEDER
+                    </button>
+                    </form>
+                </fieldset>
+    
+        </div>
+    </body>
+</html>
